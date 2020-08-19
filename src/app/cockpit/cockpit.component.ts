2bd8f6db112426439c01eebe05ce0092682c1750
Mike Lofgren <mikeylofgren@gmail.com>
2020-08-19 12:03:37 -0700

    add property binding.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cockpit',
-  templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"],
 })
 export class CockpitComponent implements OnInit {
+  newServerName = "";
+  newServerContent = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddServer() {
+    // this.serverElements.push({
+    //   type: "server",
+    //   name: this.newServerName,
+    //   content: this.newServerContent,
+    // });
   }
 
+  onAddBlueprint() {
+    // this.serverElements.push({
+    //   type: "blueprint",
+    //   name: this.newServerName,
+    //   content: this.newServerContent,
+    // });
+  }
 }
